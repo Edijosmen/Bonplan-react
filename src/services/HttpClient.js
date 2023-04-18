d0e6296290ca742aeea4305dd9ff4b3c1129689d
Edijosmen <naruto-capitulo1@hotmail.com>
2023-04-17 21:23:09 -0500

    formulario imagen y nuevo producto

--- src/services/HttpClient.js
+++ src/services/HttpClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//axios.defaults.baseURL = 'https://localhost:7146/api/';
-axios.defaults.baseURL = 'https://e376-190-2-211-106.ngrok-free.app/api/';
+axios.defaults.baseURL = 'https://localhost:7146/api/';
+// axios.defaults.baseURL = 'https://e376-190-2-211-106.ngrok-free.app/api/';
 
 axios.interceptors.request.use((config)=>{
     const token = window.localStorage.getItem('security_token');
